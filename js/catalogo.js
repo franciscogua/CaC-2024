44afa5280ec136eb9890383593f4f1fe8de1990d
valentina <valencanosa29@gmail.com>
2024-05-28 21:55:12 -0300

    agregado de fetch

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,57 +1,22 @@
-const libros = [
-  {
-    id: 1,
-    titulo: "H!STÉRICA",
-    genero: "Poesía",
-    URL_portada: "./images/libros/portada-libro7.jpg",
-  },
-  {
-    id: 2,
-    titulo: "CERROJO",
-    genero: "Poesía",
-    URL_portada: "./images/libros/portada-libro8.jpg",
-  },
-  {
-    id: 3,
-    titulo: "SILENCIO",
-    genero: "Poesía",
-    URL_portada: "./images/libros/portada-libro11.jpg",
-  },
-  {
-    id: 4,
-    titulo: "LUZ Y FUERZA",
-    genero: "Poesía",
-    URL_portada: "./images/libros/portada-libro10.jpg",
-  },
-  {
-    id: 5,
-    titulo: "REALIDARK",
-    genero: "Poesía",
-    URL_portada: "./images/libros/portada-libro9.jpg",
-  },
-  {
-    id: 6,
-    titulo: "La lógica de lo perecedero",
-    genero: "Plaqueta",
-    URL_portada: "./images/libros/portada-libro2.jpg",
-  },
-  {
-    id: 7,
-    titulo: "Ojas",
-    genero: "Plaqueta",
-    URL_portada: "./images/libros/portada-libro3.jpg",
-  },
-  {
-    id: 8,
-    titulo: "Polvareda",
-    genero: "Plaqueta",
-    URL_portada: "./images/libros/portada-libro5.jpg",
-  },
-];
-
 const catalogContainer = document.getElementById("catalog-container");
 
-libros.forEach((item) => {
+fetch("./libros.json")
+.then(datos => {
+   if(!datos.ok){
+    throw new Error("Error al traer los datos");
+   } else {
+    return datos.json();
+   }
+}).then(libros => {
+    libros.catalogo.forEach(libro => {
+        mostrarLosLibros(libro); // Ensure this matches the function name
+    });
+})
+.catch(e => {
+    console.error("Hubo un error al operar con fetch " + e.message);
+});
+
+const mostrarLosLibros = (libro) => { 
   const itemContainer = document.createElement("div");
   itemContainer.classList.add("item-container");
 
@@ -59,18 +24,18 @@ libros.forEach((item) => {
   bookContainer.classList.add("book-container");
 
   const link = document.createElement("a");
-  link.href = `./pages/detail/${item.id}.html`;
+  link.href = `./pages/detail/${libro.id}.html`; 
 
   const img = document.createElement("img");
-  img.src = item.URL_portada;
-  img.alt = item.titulo;
+  img.src = libro.URL_portada;
+  img.alt = libro.titulo; 
   img.classList.add("product-item");
 
   const title = document.createElement("h5");
-  title.textContent = item.titulo;
+  title.textContent = libro.titulo; 
 
   const genre = document.createElement("p");
-  genre.textContent = item.genero;
+  genre.textContent = libro.genero; 
 
   link.appendChild(img);
   bookContainer.appendChild(link);
@@ -78,4 +43,4 @@ libros.forEach((item) => {
   itemContainer.appendChild(title);
   itemContainer.appendChild(genre);
   catalogContainer.appendChild(itemContainer);
-});
+};
